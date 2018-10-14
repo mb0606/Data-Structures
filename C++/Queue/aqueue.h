bc386683a1ae2f3213ccd82412209e995b5db6b9
Marco Berardini <mberardini@evolution-man.com>
2018-10-13 20:43:05 -0700

    Update dequeue and added copy constructor

--- C++/Queue/aqueue.h
+++ C++/Queue/aqueue.h
@@ -10,14 +10,28 @@ private:
 
 
 public:
-	AQueue(int max = 50) {  // Constructor
-							// Make list array one position larger for empty slot maxSize = size+1;
+	explicit AQueue(int max = 50) {  // Constructor
+									 // Make list array one position larger for empty slot maxSize = size+1;
 		rear = 0;
 		front = 1;
 		size = 0;
 		maxSize = 50;
 		listArray = new E[maxSize];
 	}
+
+	//copy constructor
+	AQueue(const AQueue<E>& copyObj) {
+		rear = copyObj.rear;
+		front = copyObj.front;
+		size = copyObj.size;
+		maxSize = copyObj.maxSize;
+		listArray = new E[maxSize];
+		for (int i = copyObj.front; i <= copyObj.rear; i++) {
+			listArray[i] = copyObj.listArray[i];
+		}
+
+	}
+
 	~AQueue() {
 		delete[] listArray;
 	} // Destructor
@@ -27,24 +41,27 @@ public:
 		front = 1;
 	} // Reinitialize
 	void enqueue(const E& it) {     // Put "it" in queue
-									//Assert(((rear+2) % maxSize) != front, "Queue is full");
 		rear = (rear + 1) % maxSize;       // Circular increment
 		listArray[rear] = it;
 		size++;
 	}
 	E dequeue() {           // Take element out
-							//Assert(length() != 0, "Queue is empty");
 		E it = listArray[front];
-		front = (front + 1) % maxSize; 
-		size++;// Circular increment
+		if (length() == 1) {
+			clear();
+		}
+		//Assert(length() != 0, "Queue is empty");
+		else if (front < rear) {
+			front = (front + 1) % maxSize;
+			size++;// Circular increment
+		}
 		return it;
 	}
 	const E& frontValue() const {  // Get front value
-								   //Assert(length() != 0, "Queue is empty");
 		return listArray[front];
 	}
 	bool isEmpty() {
-		return front >= rear;
+		return front > rear;
 	}
 	virtual int length() const         // Return length
 	{
@@ -52,7 +69,7 @@ public:
 	}
 	void printValues() {
 		for (int i = 0; i < size; i++) {
-			cout << listArray[front +i ].name << endl;
+			std::cout << listArray[front + i].name << std::endl;
 		}
 	}
 
